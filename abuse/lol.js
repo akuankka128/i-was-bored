1ee19b513a3340b9a5cf3a04b07253a2c0a13999
akuankka128 <30865373+akuankka128@users.noreply.github.com>
2020-05-31 18:21:49 +0300

    Add files via upload

---
+++ abuse/lol.js
@@ -0,0 +1,33 @@
+[][
+	(Array.prototype.filter.toString()).replace(/.........(.)........................./, '$1').toLowerCase()
+	+ (Array.prototype.filter.toString()).replace(/..........(.)......................../, '$1').toLowerCase()
+	+ (Array.prototype.filter.toString()).replace(/...........(.)......................./, '$1').toLowerCase()
+	+ (Array.prototype.filter.toString()).replace(/............(.)....................../, '$1').toLowerCase()
+	+ (Array.prototype.filter.toString()).replace(/.............(.)...................../, '$1').toLowerCase()
+	+ (Array.prototype.filter.toString()).replace(/..............(.)..................../, '$1').toLowerCase()
+][
+	(Array.prototype.concat.toString()).replace(/.........(...)......................./, '$1').toLowerCase()
+	+ (Array.prototype.includes.toString()).replace(/....(.)...........(.)..................../, '$2$1').toLowerCase()
+	+ (Array.prototype.reduceRight.toString()).replace(/....(.)....(.)..(.)(.)...........(.)............../, '$2$3$4').toLowerCase()
+	+ (Array.prototype.toLocaleString.toString()).replace(/....(.).(.)............(.)......................./, '$1$2$3').toLowerCase()
+](
+(
+	Array.prototype.entries.toString()).replace(/.........(.).(.)(.)......................./, '$3$1$2').toLowerCase()
+	+ (Array.prototype.reduceRight.toString()).replace(/..(.).....(.)...(.)..(.)......................../, '$3$4$1$2').toLowerCase()
+	+ (Array.prototype.copyWithin.toString()).replace(/....(.)(.)..........(.)....................../, '$1$3$2').toLowerCase()
+	+ (Array.prototype.shift.toString()).replace(/.........(.)......................../, '$1').toLowerCase()
+)()[
+	(Array.prototype.concat.toString()).replace(/.........(...)......................./, '$1').toLowerCase()
+	+ (Array.prototype.entries.toString()).replace(/...............(.)..................../, '$1').toLowerCase()
+	+ (Array.prototype.concat.toString()).replace(/.......(.)..(.)......................../, '$2').toLowerCase()
+	+ (Array.prototype.flat.toString()).replace(/..........(.)....................../, '$1').toLowerCase()
+	+ (Array.prototype.__defineGetter__.toString()).replace(/............(.)................................/, '$1').toLowerCase()
+][
+	(Array.prototype.fill.toString()).replace(/............(.)..................../, '$1').toLowerCase()
+	+ (Array.prototype.join.toString()).replace(/......(.)........................../, '$1').toLowerCase()
+	+ (Array.prototype.__lookupGetter__.toString()).replace(/.................(.).........................../, '$1').toLowerCase()
+](
+	(Array.prototype.fill.toString()).replace(/...........(.)...................../, '$1').toLowerCase()
+	+ (Array.prototype.concat.toString()).replace(/......(.)............................/, '$1').toLowerCase()
+	+ (Array.prototype.filter.toString()).replace(/...........(.)......................./, '$1').toLowerCase()
+)
