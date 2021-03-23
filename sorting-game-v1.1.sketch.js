6ed20a42ff256dd869978b87c08e32d920a179da
akuankka128 <30865373+akuankka128@users.noreply.github.com>
2021-03-23 15:17:06 +0200

    Update and rename sorting-game.sketch.js to sorting-game-v1.1.sketch.js

--- sorting-game-v1.1.sketch.js
+++ sorting-game-v1.1.sketch.js
@@ -1,7 +1,16 @@
+// CONTROLS:
+//  Spacebar - swap
+//
+// OBJECTIVE:
+//  Sort the array with
+//  the least amount of
+//  swaps and time possible
+
 let values
 let cols = 10
 let mul
 let sel = 0
+let start = false
 let stats = {
   swaps: 0
 }
@@ -24,6 +33,7 @@ function draw() {
   
   if(mutated) {
     background(0);
+    
     let min = values[0]
     let flag = false
     
@@ -47,11 +57,19 @@ function draw() {
       rect(mul * i, height, mul, -values[i])
     }
     
+    if(start) {
+      text(`Running time: ${Date.now() - start}ms`, 5, 30)
+    }
+    
     mutated = false
   }
 }
 
 function keyPressed() {
+  if(!start) {
+    start = Date.now()
+  }
+  
   switch(keyCode) {
     case RIGHT_ARROW:
       sel += 1
@@ -63,13 +81,15 @@ function keyPressed() {
       sel *= sel >= 0
       mutated = true
       break
-    case 81:
+    case 32: // ' '
       if(sel <= 0) {
         return
       }
 
       swap(values, sel, sel - 1)
       mutated = true
+        
+      break
   }
 }
   
